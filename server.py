471a31f4a44c729671537d5b930198614b6a2cb8
vivek <viveksivaram00@gmail.com>
2021-06-21 21:11:48 +0100

    stock_api_v1

---
+++ server.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from fastapi import UploadFile , File
+import uvicorn
+from fastapi.encoders import jsonable_encoder
+from stock import *
+from pydantic import BaseModel
+from typing import Optional
+
+
+
+
+
+class Item(BaseModel):
+    ticker: str
+    preddays: str
+    epochs : Optional[int] = 2
+
+
+
+
+app = FastAPI()
+
+
+
+
+
+@app.post('/')
+async def predict_image(item:Item):
+        setparam(item.epochs)
+        print(item.epochs)
+        createdata = create_data(item.ticker,int(item.preddays))
+        modeltrain = train(createdata,item.epochs)
+        result =  str(predict(createdata))
+
+
+        return result
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0')
+
+
+
